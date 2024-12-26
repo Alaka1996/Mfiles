350aafef286cbaa160628d6e4c4fd0510a2afc38
Alaka1996 <bgalakananda@gmail.com>
2024-12-26 18:38:05 +0530

    Update test_sensor.c

--- test_sensor.c
+++ test_sensor.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void test_average_basic() {
-    uint16_t data[] = {100, 200, 300, 400, 500};
+    uint16_t data[] = {100, 200, 300, 400, 600};
     int avg = calculate_average(data, 5);  // Use the function from utils.c
     assert(avg == 300);
     printf("Test 1 passed: Basic average calculation\n");
