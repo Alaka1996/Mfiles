abe975d462e0e07487c51cd5441d923024eb0a0b
alaka <bgalakananda@gmail.com>
2024-12-26 11:29:07 +0530

    update

--- test_sensor.c
+++ test_sensor.c
@@ -1,8 +1,7 @@
-#include <assert.h>
+c#include <assert.h>
 #include <stdint.h>
 
-// Mock data processing function
-int mock_calculate_average(uint16_t *data, int size) {
+int calculate_average(uint16_t *data, int size) {
     int sum = 0;
     for (int i = 0; i < size; i++) {
         sum += data[i];
@@ -10,12 +9,14 @@ int mock_calculate_average(uint16_t *data, int size) {
     return sum / size;
 }
 
+
 int main() {
+    void test_average() {
     uint16_t data[] = {100, 200, 300, 400, 500};
-    int avg = mock_calculate_average(data, 5);
-    
-    // Verify average is as expected
+    int avg = calculate_average(data, 5);
     assert(avg == 300);
+}
+
 
     // If no assertion fails, test passes
     return 0;
